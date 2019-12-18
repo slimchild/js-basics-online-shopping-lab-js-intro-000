5cd8b0d34ae5303f0151bfc7e215d0cdf32f9ced
Timothy Wyld <timwyld@gmail.com>
2019-12-18 23:57:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var cart = [];
+const item = {
+  itemName: itemName,
+  itemPrice: itemPrice
+};
+
 
 function getCart() {
  return cart;
@@ -10,12 +15,29 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push({itemName:item,itemPrice:Math.floor(Math.random() * 100),});
+return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
-}
+   if(cart.length === 0){
+   return "Your shopping cart is empty."
+ }
+ }
+
+
+  //var values = Object.values(cart);
+//  let blah = "";
+  //for(var i = 0; i < cart.length; i++){
+//blah += `${itemName} ${itemPrice}`;
+    //blah += keys[i] + " at" + values[i] + ",";
+    //if(i === cart.length-1){
+      //blah += ". ";
+
+    //return keys;
+  //}
+
+  //return "In your cart you have " + blah;
 
 function total() {
   // write your code here
